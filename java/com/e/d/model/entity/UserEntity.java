48f271afa453c1ab20044d176ec3d350fa02713f
Da yoon <gangd0642@gmail.com>
2024-10-23 23:59:20 +0900

    Update UserEntity.java

--- java/com/e/d/model/entity/UserEntity.java
+++ java/com/e/d/model/entity/UserEntity.java
@@ -1,15 +1,7 @@
 package com.e.d.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "TESTUSER")
